5f816b5902d36333005df21c21add8c28f9bf415
Marcin Olszowski <olszyna38@gmail.com>
2019-06-18 23:20:33 +0200

    Updates

---
+++ precache-manifest.8f39c1b7e72771d1e33bc832ac75d1d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f845c0e1602eea47d1d783bd2f915bff",
+    "url": "/staples_task/index.html"
+  },
+  {
+    "revision": "863b46ad1878ae1f8c1a",
+    "url": "/staples_task/static/css/2.58d2e77d.chunk.css"
+  },
+  {
+    "revision": "1e976fa032add4058451",
+    "url": "/staples_task/static/css/main.5a8ec12b.chunk.css"
+  },
+  {
+    "revision": "863b46ad1878ae1f8c1a",
+    "url": "/staples_task/static/js/2.cce97cbb.chunk.js"
+  },
+  {
+    "revision": "1e976fa032add4058451",
+    "url": "/staples_task/static/js/main.38f38160.chunk.js"
+  },
+  {
+    "revision": "279738e7e7cef1301a70",
+    "url": "/staples_task/static/js/runtime~main.04a5da10.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/staples_task/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/staples_task/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/staples_task/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/staples_task/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/staples_task/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
