56af80270c31ebf176ff0b6992457d24242fb644
Marcin Olszowski <olszyna38@gmail.com>
2019-06-18 22:50:56 +0200

    Add cart icon

--- client/src/Cart.js
+++ client/src/Cart.js
@@ -3,7 +3,10 @@ import React from "react";
 const Cart = props => {
   return (
     <div className="cart">
-      <div className="cart_number">{props.shoppingCart}</div>
+      <div className="cart_number">
+        {props.shoppingCart}
+        <i class="fas fa-shopping-cart" />
+      </div>
     </div>
   );
 };
